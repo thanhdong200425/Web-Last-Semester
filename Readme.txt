19d1381ca58d716c0ae843b2f8c1abc4dc3c9a98
Thanh Dong <thanhdong200425@gmail.com>
2023-06-17 11:19:56 +0700

    Build navigation

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+Các thành phần trên trang web cần làm
+ 1) Thanh navigation
+ 2) Thanh menu chính
---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+
+
+const app = {
+
+    handleEvent: () => {
+    },
+    start: () => {
+        app.handleEvent();
+    }
+};
+
+app.start();
